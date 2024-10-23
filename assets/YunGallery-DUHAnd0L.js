4ac6f1a4578888bc5f7ced035407abb5ff7e56a2
Elkeid-me <Elkeid-me@users.noreply.github.com>
2024-10-23 15:57:42 +0000

    deploy: 1e929344df522d807403a5806b9fb22b57bc5710

---
+++ assets/YunGallery-DUHAnd0L.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-ClDFoE-f.js";import"./app-D-5_sa4j.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-ONKNvQI_.js";export{o as default};
---
+++ assets/back-ChWKeyOE.js
@@ -0,0 +1,1 @@
+import{u as o}from"./app-D-5_sa4j.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
